6257a0ffb5358edb976bc710b32ee91e246df8d7
MdAfsarHossain <afsar291722@gmail.com>
2025-05-05 20:55:01 +0600

    Chef Recommends jsx file added

---
+++ src/components/ChefRecommends.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChefRecommends = () => {
+    return (
+        <div>
+            <h1>Chef Recommends</h1>
+        </div>
+    );
+};
+
+export default ChefRecommends;
